b9779a279012c7ae19fa9e595b8a42204e107404
v3ct0r22 <rekinvector@gmail.com>
2019-08-13 20:13:08 +0000

    Added a person_detail view and some test(Person && Role) data in the admin panel...

--- django/core/admin.py
+++ django/core/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 
-from .models import Movie
+from .models import Movie, Person, Role
+
+
+admin.site.register(Person)
+admin.site.register(Role)
 
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'released', 'runtime',
-                    'rating')
+                    'rating', 'director')
     list_filter = ('released', 'rating')
     search_fields = ('title', 'description')
     prepopulated_fields = {'slug': ('title',)}
--- django/core/urls.py
+++ django/core/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('movies/', views.MovieList.as_view(), name='movie_list'),
     path('movie/<int:pk>', views.MovieDetail.as_view(),
          name='movie_detail'),
+    path('person/<int:pk>', views.PersonDetail.as_view(),
+         name='person_detail'),
 ]
